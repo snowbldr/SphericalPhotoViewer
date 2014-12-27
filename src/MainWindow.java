08c4446c85431ff8cb9dea09b56a84d9276c79ef
Robert Kempton <Robert.Kempton@us.usana.com>
2014-12-27 01:03:19 -0700

    initial class creation.

---
+++ src/MainWindow.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author robertk11
+ *         Date: 12/27/14
+ *         Time: 1:01 AM
+ */
+public class MainWindow extends JFrame {
+}
---
+++ src/WallSphere.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author robertk11
+ *         Date: 12/27/14
+ *         Time: 1:03 AM
+ */
+public class WallSphere {
+}
